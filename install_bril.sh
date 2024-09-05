6923c8d728061e384f3014cae2df2dccbf37550c
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-09-05 16:56:35 +0800

    Customize `install_bril.sh` for weird time limitation

--- install_bril.sh
+++ install_bril.sh
@@ -5,4 +5,15 @@
 # Ensure the script works on any machine and sets up the PATH correctly.
 script_dir=$(realpath $(dirname $0))
 
-$script_dir/container/scripts/acd/setup.sh
+cp ${script_dir}/container/scripts/acd/.acd.sh ~
+_CMD=$(echo -e "\nsource ~/.acd.sh\n")
+echo $_CMD >> ~/.bashrc
+echo $_CMD >> ~/.zshrc
+
+pip install --user flit
+# install brili
+deno install ${BRIL_REPO}/brili.ts
+
+# install flit and bril2xxx
+pipx install flit
+cd ${BRIL_REPO}/bril-txt && flit install --symlink --user
