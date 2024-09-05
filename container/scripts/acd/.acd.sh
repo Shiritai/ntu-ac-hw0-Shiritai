3a0c32a0149bae892761b22d2d259fb26ffb548f
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-09-05 16:39:13 +0800

    Update scripts
    For
    * User that needs sudo privilege to run docker
    * Better `PATH` and running directory support

--- container/scripts/acd/.acd.sh
+++ container/scripts/acd/.acd.sh
@@ -4,4 +4,4 @@
 export DENO_INSTALL="$HOME/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.local/share/pipx/venvs/flit/bin:$PATH"
+export PATH="$(pipx list | grep venv | grep $HOME | sed 's/.* //')/flit/bin:$PATH"
--- container/scripts/acd/setup.sh
+++ container/scripts/acd/setup.sh
@@ -8,9 +8,7 @@ sudo apt update -y && \
     sudo apt install git curl unzip pipx -y
 
 # setup shell (bash and zsh)
-_ACD_SRC=${script_dir}/.acd.sh
-${_ACD_SRC} # run once to apply change immediately
-cp ${_ACD_SRC} ~
+cp ${script_dir}/.acd.sh ~
 _CMD=$(echo -e "\nsource ~/.acd.sh\n")
 echo $_CMD >> ~/.bashrc
 echo $_CMD >> ~/.zshrc
@@ -21,6 +19,12 @@ BRIL_REPO=~/repos/bril
 mkdir -p $BRIL_REPO
 sudo git clone https://github.com/sampsyo/bril.git $BRIL_REPO
 curl -fsSL https://deno.land/install.sh | sh
+
+# temporary setup deno path
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+
 deno install ${BRIL_REPO}/brili.ts
 pipx install flit
-cd ${BRIL_REPO}/bril-txt && ~/.local/bin/flit install --symlink
+cd ${BRIL_REPO}/bril-txt && flit install --symlink
